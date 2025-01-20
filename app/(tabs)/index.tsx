927d4a7731587fb470f3def6e4816082a399911f
Lorenzo <alejandro36036@gmail.com>
2025-01-19 23:38:24 -0400

    feature: implementation of section information
    We added 4 components of type icons which will handle future reactions, the Icon menu component was added, the ranking and the input were integrated.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
-import Login from "../../view/Login";
+import Login from "@/view/Login";
+import Local from "@/view/Local"
 
 export default function index() {
   // Comprobar si el usuario ya esta logeado
 
   //
 
-  return <Login />;
+  return <Local />;
 }
