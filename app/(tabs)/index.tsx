66ca99471e06710fa59c8ea81ee180fd22e3441f
Lorenzo <alejandro36036@gmail.com>
2025-01-20 21:48:33 -0400

    feature: IndexRevert
    : IndexRevert

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
-import Login from "@/view/Login";
-import Local from "@/view/Local"
+import Login from "../../view/Login";
+
 
 export default function index() {
   // Comprobar si el usuario ya esta logeado
 
   //
 
-  return <Local />;
+  return <Login />;
 }
