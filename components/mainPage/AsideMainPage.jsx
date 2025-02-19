244123a677bd0b5548918360cd37a2e93cd473d9
Franmari Garcia <franmarigarciafuentes@gmail.com>
2025-02-18 22:34:40 -0400

    Update AsideMainPage.jsx

--- components/mainPage/AsideMainPage.jsx
+++ components/mainPage/AsideMainPage.jsx
@@ -1,173 +1,102 @@
-import React, { useEffect, useState, useRef } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Image,
-  Animated,
-  useAnimatedValue,
-  Pressable,
-  Easing,
-} from "react-native";
-
-import Icon from "@expo/vector-icons/Feather";
-import Colors from "../../constants/Colors";
-// import Icon from "react-native-vector-icons/MaterialIcons";
-import { useRouter } from "expo-router";
-// import { withSpring, useSharedValue } from "react-native-reanimated";
-
-const WIDTH_ASIDE = 275;
-
-const styles = StyleSheet.create({
-  iconButton: {
-    position: "absolute",
-    top: 20,
-    left: 20,
-    backgroundColor: Colors.primary,
-    padding: 10,
-    borderRadius: 50,
-    zIndex: 10,
-  },
-  image: {
-    width: "100%",
-    resizeMode: "contain",
-  },
-  imageContainer: {
-    width: "100%",
-  },
-  aside: {
-    backgroundColor: Colors.primary,
-    width: WIDTH_ASIDE, // Usar porcentaje en lugar de dvw
-    height: "100%",
-    paddingTop: 80,
-    flexDirection: "column",
-    borderWidth: 1,
-    borderTopColor: "transparent",
-    borderBottomColor: "transparent",
-    borderLeftColor: "transparent",
-    borderRightColor: Colors.whiteTransparent,
-    zIndex: 10,
-    position: "absolute",
-    top: 0,
-    left: 0,
-  },
-  textAsideContainer: {
-    padding: 15,
-    paddingLeft: 20,
-  },
-  textAside: {
-    color: "#f5f5f5",
-    fontSize: 18,
-  },
-  textAsidePressed: {
-    backgroundColor: "rgba(255, 255, 255, 0.2)", // Fondo más oscuro al presionar
-  },
-});
+import React from "react";
+import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { Image } from "expo-image";
 
 export default function AsideMainPage({ visible, setVisible }) {
-  const animation = useRef(new Animated.Value(-WIDTH_ASIDE)).current;
-  const navigation = useRouter();
-  const [pressedItem, setPressedItem] = useState(null); // Estado para el item presionado
+  const [fontsLoaded] = useFonts({
+    "League-Gothic": require("../../assets/fonts/LeagueGothic-Regular.ttf"),
+    "Open-Sans": require("../../assets/fonts/OpenSans-Regular.ttf"),
+  });
 
-  const handlePressIn = (item) => {
-    setPressedItem(item);
-  };
-
-  const handlePressOut = () => {
-    setPressedItem(null);
-  };
-
-  const handlePress = (route) => {
-    navigation.push(route); // Navegar a la ruta
-  };
-
-  //----------------------------AQUI LAS RUTAS DEL ASIDE
-  const menuItems = [
-    { label: "Historial", route: "/historial" },
-    { label: "Me gusta", route: "/megusta" },
-  ];
-
-  const showAside = () => {
-    Animated.timing(animation, {
-      toValue: 0,
-      duration: 200,
-      easing: Easing.in(Easing.ease),
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const hideAside = () => {
-    Animated.timing(animation, {
-      toValue: -WIDTH_ASIDE,
-      duration: 200,
-      easing: Easing.out(Easing.ease),
-      useNativeDriver: true,
-    }).start();
-  };
-
-  useEffect(() => {
-    if (visible) {
-      showAside();
-    } else {
-      hideAside();
-    }
-    console.log("visible", visible);
-    console.log("animation", animation);
-  }, [visible]);
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <>
-      {visible && (
-        <Pressable
-          onPress={() => {
-            setVisible(false);
-            hideAside();
-          }}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-        />
-      )}
-      <Animated.View
-        style={[styles.aside, { transform: [{ translateX: animation }] }]}
-      >
-        <Pressable
-          style={{
-            position: "absolute",
-            top: 30,
-            right: 20,
-          }}
-          onPress={() => {
-            setVisible(!visible);
-          }}
-        >
-          <Icon name="x" size={40} color="white" />
-        </Pressable>
-
-        <Image
-          source={require("@/assets/images/logo.jpg")}
-          style={styles.image}
-        />
-        {menuItems.map((item) => (
-          <TouchableOpacity
-            key={item.label}
-            style={[
-              styles.textAsideContainer,
-              pressedItem === item.label && styles.textAsidePressed, // Estilo al presionar
-            ]}
-            onPressIn={() => handlePressIn(item.label)}
-            onPressOut={handlePressOut}
-            onPress={() => handlePress(item.route)} // Navegación al presionar
-          >
-            <Text style={styles.textAside}>{item.label}</Text>
+    <Modal transparent visible={visible} animationType="fade">
+      <View style={styles.overlay}>
+        <TouchableOpacity style={styles.background} onPress={() => setVisible(false)} />
+        <View style={styles.menu}>
+          <View style={styles.header}>
+            <Image source={require("@/assets/images/logo_recortado.png")} style={styles.logo} />
+            <Text style={styles.title}>FOODIGO</Text>
+          </View>
+          
+          <TouchableOpacity style={styles.backButton} onPress={() => setVisible(false)}>
+            <Image source={require("@/assets/images/asidemainbutton.png")} style={styles.backImage} />
           </TouchableOpacity>
-        ))}
-      </Animated.View>
-    </>
+
+          <TouchableOpacity style={styles.menuItem} onPress={() => setVisible(false)}>
+            <Text style={styles.menuText}>Perfil</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem} onPress={() => setVisible(false)}>
+            <Text style={styles.menuText}>Historial</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem} onPress={() => setVisible(false)}>
+            <Text style={styles.menuText}>Me gusta</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuItem} onPress={() => setVisible(false)}>
+            <Text style={styles.menuText}>Local</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.7)", // Fondo semitransparente negro
+  },
+  background: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  menu: {
+    width: "45%",
+    marginTop: 30,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 30,
+  },
+  logo: {
+    width: 60,  
+    height: 100,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 100,
+    color: "#FFF",
+    fontFamily: "League-Gothic",
+  },
+  backButton: {
+    marginBottom: 40,
+  },
+  backImage: {
+    width: 40, 
+    height: 40,
+  },
+  menuItem: {
+    width: "100%",
+    padding: 8,
+    borderWidth: 2,
+    borderColor: "#FFF",
+    borderRadius: 5,
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  menuText: {
+    color: "#FFF",
+    fontSize: 12,
+    fontFamily: "Open-Sans",
+  },
+});
